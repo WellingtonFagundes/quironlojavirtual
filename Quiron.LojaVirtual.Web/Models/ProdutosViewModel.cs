58d70ff118de496fa070ac705e2f83f2d8f704b3
wellingtonfagundes <wellington.life@hotmail.com>
2015-11-04 20:15:28 -0200

    Aula 12 - Partial e Bootstrap

---
+++ Quiron.LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    //Classe que representa informações do domínio
+    public class ProdutosViewModel
+    {
+        public IEnumerable<Produto> Produtos { get; set; }
+
+        public Paginacao Paginacao { get; set; }
+    }
+}
