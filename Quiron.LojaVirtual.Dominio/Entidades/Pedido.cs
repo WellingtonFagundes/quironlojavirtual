621aaa76b4c43c0a683ef395fce064a4899c2983
wellingtonfagundes <wellington.life@hotmail.com>
2015-11-09 19:39:11 -0200

    Pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe seu nome")]
+        public string NomeCliente { get; set; }
+
+        
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+        
+        [Required(ErrorMessage="Informe o endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        
+        [Required(ErrorMessage="Informe a cidade")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+
+        [Required(ErrorMessage="Informe o bairro")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        
+        [Required(ErrorMessage="Informe o Estado")]
+        [Display(Name = "Estado:")]
+        public string Estado { get; set; }
+
+        
+        [Required(ErrorMessage="Informe o Email")]
+        [Display(Name="Email:")]
+        [EmailAddress(ErrorMessage="Email Inválido")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+    }
+}
