5e908949906f2f8f965c7a35a44571e6ebda6609
wellingtonfagundes <wellington.life@hotmail.com>
2015-11-14 19:12:12 -0200

    Tela de Manutenção de Produtos

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Produto
     {
+        //Para ocultar o produtoId
+        [HiddenInput(DisplayValue=false)]
         public int ProdutoId { get; set; }
 
         public string Nome { get; set; }
 
+        //Para criar um TextArea
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
 
         public decimal Preco { get; set; }
