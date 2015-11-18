b426f5d02a0b55d7a1411227dfadd0b584520865
wellingtonfagundes <wellington.life@hotmail.com>
2015-11-18 14:41:43 -0200

    Classe Administradores

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Administrador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Administrador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o Login")]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        public DateTime UltimoAcesso { get; set; }
+    }
+}
