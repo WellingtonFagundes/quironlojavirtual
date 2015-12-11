f403e7512fef3f10012f1a98ddca881f617d54f5
wellingtonfagundes <wellington.life@hotmail.com>
2015-12-11 12:23:02 -0200

    Implementação ObterProdutosVitrine

--- Quiron.LojaVirtual.Web.V2/Controllers/NavController.cs
+++ Quiron.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
 using Quiron.LojaVirtual.Web.V2.Models;
 
 namespace Quiron.LojaVirtual.Web.V2.Controllers
@@ -16,12 +17,27 @@ namespace Quiron.LojaVirtual.Web.V2.Controllers
             return View();
         }
 
+        public JsonResult TesteMetodoVitrine()
+        {
+            ProdutoModeloRepositorio repositorio = new ProdutoModeloRepositorio();
+
+            //var produtos = repositorio.ObterProdutosVitrine(categoria: "0083", marca: "1106");
+
+            var produtos = repositorio.ObterProdutosVitrine(modalidade: "0051");
+
+            
+            return Json(produtos, JsonRequestBehavior.AllowGet);
+        }
+
+
         [Route("nav/{id}/{marca}")]
         public ActionResult ObterProdutosPorMarcas(string id)
         {
-            var _model = new ProdutosViewModel {Produtos = null};
+           // ProdutoModeloRepositorio repositorio = new ProdutoModeloRepositorio();
 
-            return View("Index", _model);
+
+
+            return View("Index", null);
         }
 	}
 }
